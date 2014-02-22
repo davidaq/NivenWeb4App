81956c694858031e6ce978c414cade801e5c00b7
DavidAQ <153019747@qq.com>
2014-02-22 19:51:56 +0800

    initial

---
+++ Readme.mk
@@ -0,0 +1,7 @@
+===== Niven Web for App =====
+
+Writing the HTTP part for an app that uses a webservice is absolutely painful.
+
+This is the solution that will alow you to write simple RESTful server side services and generate code for client side (Android an iOS which I am familiar with).
+
+The aim is not to be a powerful framework as Spring or Struts, but to be a simple to use solution for small scaled project.
